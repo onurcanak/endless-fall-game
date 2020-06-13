e47165d3e8dd3366b828a0deaf00e3b580434535
Onur Can Ak <softargedev1@outlook.com>
2020-06-14 01:32:45 +0300

    added d3 for creating svg elements & transition

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 
     //#region global variables
 
-    let gameSpeed = 1000;        // game speed in ms
+    let gameSpeed = 10;             // game speed in ms (scrolling effect)
+    let blockGenerateSpeed = 500;   // block generating speed in ms
     let gameArea_height = $('#gameArea').height();     // height of the game area
     let gameArea_width  = $('#gameArea').width();      // width of the game area
     
@@ -22,54 +23,53 @@ $(document).ready(function(){
      */
     function generateNewStepBlock(){
 
-        /**
-         * generate first block's width randomly. the ball should be able to pass between the blocks, so we need to substract the ball width 
-         */
+        // generate first block's width randomly. the ball should be able to pass between the blocks, so we need to substract the ball width 
         let firstBlock_width = Math.random() * (gameArea_width - 2 * gameBall_radius);
-
-        let firstBlock = $(document.createElementNS('http://www.w3.org/2000/svg', 'rect')).attr({
-            class  : 'gameBlock',
-            height : gameBlock_height,
-            width  : firstBlock_width,
-            x      : 0,
-            y      : gameArea_height,
-            fill   : gameBlock_color
-        });
         
-        /**
-         * second block's width must be equal to (game area width - width of the first block - width of the ball)
-         */
+        // second block's width must be equal to (game area width - width of the first block - width of the ball)
         let secondBlock_width = gameArea_width - firstBlock_width - (2 * gameBall_radius);
-        let secondBlock = $(document.createElementNS('http://www.w3.org/2000/svg', 'rect')).attr({
-            class  : 'gameBlock',
-            height : gameBlock_height,
-            width  : secondBlock_width,
-            x      : gameArea_width - secondBlock_width,
-            y      : gameArea_height,
-            fill   : gameBlock_color
-        });
-       
-        return [firstBlock, secondBlock];
 
+        var svgContainer = d3.select('#gameArea');
+
+        // appending left rectangle
+        svgContainer.append('rect')
+                        .attr('class', 'gameBlock')
+                        .attr('height', gameBlock_height)
+                        .attr('width', firstBlock_width)
+                        .attr('x', 0)
+                        .attr('y', gameArea_height)
+                        .attr('fill', gameBlock_color)
+
+        // appending right rectangle
+        svgContainer.append('rect')
+                        .attr('class', 'gameBlock')
+                        .attr('height', gameBlock_height)
+                        .attr('width', secondBlock_width)
+                        .attr('x', gameArea_width - secondBlock_width)
+                        .attr('y', gameArea_height)
+                        .attr('fill', gameBlock_color)
+        
     }
 
 
-    let update = setInterval(() => {
+    let update_generateBlock = setInterval(() => {
         
-        let randomBlocks = generateNewStepBlock();
+        // generating new blocks
+        generateNewStepBlock();
 
-        $('#gameArea').append(randomBlocks[0]);
-        $('#gameArea').append(randomBlocks[1]);
+        
+    }, blockGenerateSpeed);
 
-       /*  $('.gameBlock').each(function(){
-            let currentY = parseInt($(this).attr('y'));
-            $(this).attr('y', currentY - 100)
-        }) */
 
-        $('.gameBlock').each(function(){
-            let currentY = parseInt($(this).attr('y'));
-            $(this).attr('y', currentY - 100)
-        })
+    let update_scrollEffect = setInterval(() => {
+        
+        d3.selectAll('.gameBlock')
+            .transition()
+            .attr('y', function(){
+                let currentY = parseInt($(this).attr('y'));
+                return currentY - 500;
+            })
+
         
     }, gameSpeed);
 
