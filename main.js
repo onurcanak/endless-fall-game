e172021b560a56186a594b5359e9eafa42c93d6d
Onur Can Ak <softargedev1@outlook.com>
2020-06-14 03:14:47 +0300

    added basics of the ball control

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+let update_handleWithBlocks;
+let update_scrollEffect;
+let update_ballFallingEffect;
+
 $(document).ready(function(){
 
     //#region global variables
@@ -8,9 +13,11 @@ $(document).ready(function(){
     let gameArea_width  = $('#gameArea').width();      // width of the game area
     
     let gameBlock_height = gameArea_height / 40;        // height of the blocks
-    let gameBlock_color  = 'black';
+    let gameBlock_color  = 'black';                     // color of the blocks
+    let gameBlock_movementY = 400;                      // determines how far blocks will be shifted
 
     let gameBall_radius  = gameArea_width / 30;         // radius of the ball
+    let gameBall_color  = 'red';                     // color of the blocks
 
     //#endregion
 
@@ -18,16 +25,15 @@ $(document).ready(function(){
     //#region global functions
 
     /**
-     *  generates block pairs to append to the new step 
-     *  @return two blocks as DOM elements
+     *  generates block pairs and appends them to the svg
      */
     function generateNewStepBlock(){
 
         // generate first block's width randomly. the ball should be able to pass between the blocks, so we need to substract the ball width 
-        let firstBlock_width = Math.random() * (gameArea_width - 2 * gameBall_radius);
+        let firstBlock_width = Math.random() * (gameArea_width - 3 * gameBall_radius);
         
         // second block's width must be equal to (game area width - width of the first block - width of the ball)
-        let secondBlock_width = gameArea_width - firstBlock_width - (2 * gameBall_radius);
+        let secondBlock_width = gameArea_width - firstBlock_width - (3 * gameBall_radius);
 
         var svgContainer = d3.select('#gameArea');
 
@@ -52,27 +58,100 @@ $(document).ready(function(){
     }
 
 
-    let update_generateBlock = setInterval(() => {
-        
-        // generating new blocks
-        generateNewStepBlock();
+    /**
+     *  deletes the blocks that outside the svg
+     */
+    function deleteOutsideBlocks(){
+        $('.gameBlock').each(function(){
+            let blockY = parseInt($(this).attr('y'));
+            if(blockY < 0) $(this).remove();
+        })
+    }
 
-        
-    }, blockGenerateSpeed);
 
+    /**
+     *  generates the game ball
+     */
+    function generateTheBall(){
+        d3.select('#gameArea').append('circle')
+                        .attr('id', 'gameBall')
+                        .attr('cx', gameArea_width / 2)
+                        .attr('cy', '100')
+                        .attr('r', gameBall_radius)
+                        .attr('fill', gameBall_color)
+    }
 
-    let update_scrollEffect = setInterval(() => {
-        
-        d3.selectAll('.gameBlock')
-            .transition()
-            .attr('y', function(){
-                let currentY = parseInt($(this).attr('y'));
-                return currentY - 500;
+    /**
+     *  ball controls. user can move the ball with using 'WASD' keys
+     */
+    function bindBallControl(){
+        $('html').one('keydown', function(e){
+
+            let update_move = setInterval(() => {
+                let gameBallX = parseInt($('#gameBall').attr('cx'));
+    
+                if(e.code == 'KeyA'){
+                    $('#gameBall').attr('cx', gameBallX - 3);
+                }
+                if(e.code == 'KeyD'){
+                    $('#gameBall').attr('cx', gameBallX + 3);
+                }
             })
+    
+            $('html').one('keyup', function(e){
+                clearInterval(update_move);
+                bindBallControl();
+            })
+    
+        })
+    }
 
-        
-    }, gameSpeed);
+
+    function startTimers(){
+        /**
+         *  its used for generating/deleting blocks
+         */
+        update_handleWithBlocks = setInterval(() => {
+            
+            // generating new blocks
+            generateNewStepBlock();
+
+            // deleting the blocks that doesn't exists on the screen
+            deleteOutsideBlocks();
+
+            
+        }, blockGenerateSpeed);
+
+
+
+        /**
+         *  its used for scroll effect
+         */
+        update_scrollEffect = setInterval(() => {
+            
+            d3.selectAll('.gameBlock')
+                .transition()
+                .attr('y', function(){
+                    let currentY = parseInt($(this).attr('y'));
+                    return currentY - gameBlock_movementY;
+                })
+
+            
+        }, gameSpeed);
+    }
 
     //#endregion
+
+
+    /**
+     *  starting the game
+     */
+    (function(){
+        generateTheBall();
+        bindBallControl();
+        startTimers();
+    })()
+
+
 
 })
